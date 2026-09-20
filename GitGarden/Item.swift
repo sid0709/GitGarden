3c66b40b74953d4561d4b34b1803bebe7302db3f
omnimuh730 <omnimuh730@gmail.com>
2026-09-19 22:54:06 -0500

    Initial Commit

---
+++ GitGarden/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GitGarden
+//
+//  Created by sid on 9/19/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GitGardenTests/GitGardenTests.swift
@@ -0,0 +1,19 @@
+//
+//  GitGardenTests.swift
+//  GitGardenTests
+//
+//  Created by sid on 9/19/26.
+//
+
+import Testing
+@testable import GitGarden
+
+struct GitGardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
